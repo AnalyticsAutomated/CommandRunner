ca807457e9e38bb6401aa6f21e5d27860ce8b25b
Daniel Buchan <clone@myself.com>
2015-06-30 17:09:43 +0100

    corrected setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='runner',
+setup(name='commandRunner',
       version='0.1',
       description='Allows object oriented running of code',
-      url='https://github.com/AnalyticsAutomated/Runner.git',
+      url='https://github.com/AnalyticsAutomated/commandRunner.git',
       author='Analytics Automated',
       author_email='daniel.buchan@ucl.ac.uk',
       license='GPL',
-      packages=['runner'],
+      packages=['commandRunner'],
       zip_safe=False)
