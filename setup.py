d354cc1dcfc87255d2dd336e1839cf2625c73e35
Daniel Buchan <clone@myself.com>
2015-06-30 16:48:14 +0100

    rearrange for packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='runner',
+      version='0.1',
+      description='Allows object oriented running of code',
+      url='https://github.com/AnalyticsAutomated/Runner.git',
+      author='Analytics Automated',
+      author_email='daniel.buchan@ucl.ac.uk',
+      license='GPL',
+      packages=['runner'],
+      zip_safe=False)
