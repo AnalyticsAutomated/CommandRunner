a49641afe753ea4f0de92f9d69c661f9939e9abd
Daniel Buchan <clone@myself.com>
2015-10-09 16:10:00 +0100

    making ge integration work

--- commandRunner/geRunner.py
+++ commandRunner/geRunner.py
@@ -66,14 +66,14 @@ class geRunner(commandRunner.commandRunner):
         try:
             with drmaa.Session() as s:
                 jt = s.createJobTemplate(WORKING_DIRECTORY=self.tmp_path)
-                jt.remoteCommand = self.command
-                jt.args = self.args_set
-                jt.joinFiles = True
-
-                jobid = s.runJob(jt)
-
-                retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
-                s.deleteJobTemplate(jt)
+                # jt.remoteCommand = self.command
+                # jt.args = self.args_set
+                # jt.joinFiles = True
+                #
+                # jobid = s.runJob(jt)
+                #
+                # retval = s.wait(jobid, drmaa.Session.TIMEOUT_WAIT_FOREVER)
+                # s.deleteJobTemplate(jt)
         except Exception as e:
             raise OSError("DRMAA session failed to execute:" + e)
 
