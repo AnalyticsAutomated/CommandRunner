e15e3202f675da9e43a5b267bdb432c5bf0ab2b8
Daniel Buchan <clone@myself.com>
2015-10-09 16:56:27 +0100

    making ge integration work

--- tests/test_geRunner.py
+++ tests/test_geRunner.py
@@ -16,7 +16,7 @@ class geRunnerTestCase(unittest.TestCase):
 
     # OPTIONAL
     input_string = "input.in"
-    output_string = "output.out"
+    output_string = ":output.out"
     flags = ["-lah", ]
     options = {'-a': '12', 'b': '1'}
     out_glob = ['out', ]
