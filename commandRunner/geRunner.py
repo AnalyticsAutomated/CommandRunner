d34c4624ed1bb5c1216c4c4f6b4ca76c9dfff72e
Daniel Buchan <clone@myself.com>
2015-10-09 15:29:27 +0100

    making ge integration work

--- commandRunner/geRunner.py
+++ commandRunner/geRunner.py
@@ -52,7 +52,7 @@ class geRunner(commandRunner.commandRunner):
         if self.flags is not None:
             self.args_set.extend(self.flags)
         if self.options is not None:
-            [self.args_set.extend([k, v]) for k, v in self.options.items().sorted()]
+            [self.args_set.extend([k, v]) for k, v in sorted(self.options.items())]
         if self.output_string is not None:
             self.args_set.append(self.output_string)
 
