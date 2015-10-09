98841f88e93f932de4f27941e27f58d1958704a2
Daniel Buchan <clone@myself.com>
2015-10-09 16:07:13 +0100

    making ge integration work

--- tests/test_geRunner.py
+++ tests/test_geRunner.py
@@ -62,8 +62,8 @@ class geRunnerTestCase(unittest.TestCase):
         self.assertEqual(os.path.isdir(path), True)
         self.assertEqual(os.path.exists(file1), True)
 
-    @patch('drmaa.Session.runJob', return_value=1234)
-    #@patch('commandRunner.geRunner.drmaa', return_value=0)
+    @patch('commandRunner.geRunner.drmaa.Session.runJob', return_value=1234)
+    @patch('commandRunner.geRunner.drmaa.Session.wait', return_value=0)
     def test_command_executes(self, m, n):
         r2.prepare()
         #exit_status = r2.run_cmd()
