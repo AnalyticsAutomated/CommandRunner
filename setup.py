65473f7dfd90f201d14f79537f7249b4c5391eaf
Daniel Buchan <clone@myself.com>
2016-07-07 15:01:34 +0100

    trivial change to resolve pypi versioning issue

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='commandRunner',
-      version='0.4.6',
+      version='0.4.7',
       description='Allows thread safe object oriented running of commanline operations',
       long_description=readme(),
       url='https://github.com/AnalyticsAutomated/commandRunner.git',
