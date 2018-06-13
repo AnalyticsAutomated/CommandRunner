b8992170f2230da911d2d0107f40ebb48feb085a
Daniel Buchan <clone@myself.com>
2018-06-13 17:56:25 +0100

    updated to make stdout file more writeable

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='commandRunner',
-      version='0.8.9',
+      version='0.8.10',
       description='Allows thread safe, object oriented running of commandline '
                   'operations and blocks of code',
       long_description=readme(),
